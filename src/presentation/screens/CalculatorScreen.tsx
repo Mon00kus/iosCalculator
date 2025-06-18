484ce295bb194c4b829ba515b1ea000ca0ce4ac3
mon00kus <raulmoncadac@gmail.com>
2025-06-18 13:01:22 -0400

    Initial project structure implemented
    Initial project structure implemented

---
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export const CalculatorScreen = () => {
+  return (
+    <View>
+      <Text >Ios Calculator</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+
+})
